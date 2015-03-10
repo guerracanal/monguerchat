ed867aa6ff63e54ebec0393b1606ed718c62d3a0
guerracanal <jorge.guerra.canal@gmail.com>
2015-03-10 18:05:30 +0100

    CSS responsable

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
 ﻿ 
  $(function() {
 	
-	cargar("obvio", "Cápitan Obvio", true);
-	cargar("hodor", "Hodor", true);
-	cargar("pikachu", "Pikachu", true);
+	cargar("obvio", "Cápitan Obvio", false);
+	cargar("hodor", "Hodor", false);
+	cargar("pikachu", "Pikachu", false);
 	//cargar("bocaseca", "Bocaseca", false);
 	//cargar("ganan", "Gañán", false);
 	//cargar("marlo", "Marlo", false);
-	cargar("clipo", '"Clipo" El clip del Windows', true);
-	cargar("minion", "Minion", true);
-	cargar("chiquito", "Chiquito de la Calzada", true);
+	cargar("clipo", 'El clip del Windows', false);
+	cargar("minion", "Minion", false);
+	cargar("chiquito", "Chiquito de la Calzada", false);
 	//cargar("onofre", "Onofre", false);
 	//cargar("torrente", "Torrente", false);
 	//cargar("sabina", "Joaquin Sabina", false);
@@ -17,13 +17,13 @@
 	//cargar("house","Dr House", false);
 	//cargar("barney","Barney Stinson", false);
 	//cargar("sheldon","Sheldon Cooper", false);
-	cargar("et","ET", true);
+	cargar("et","ET", false);
 	//cargar("hipster","Hipster", false);
 	//cargar("vicentin","Vicentín", false);
-	cargar("zombie","Zombie", true);
+	cargar("zombie","Zombie", false);
 	//cargar("gallu","Gallu", false);
 	//cargar("heisemberg","Heisemberg", false);
-	cargar("gatete", "Gatete", true);
+	cargar("gatete", "Gatete", false);
 	
 	$('#free').click(function(){ free(); return false; });
 	$('#history').click(function(){ history(); return false; });
